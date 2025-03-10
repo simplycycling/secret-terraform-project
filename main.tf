ba13dc277d50b18f0069a753656162f85aa2c16b
Roger Sherman <rsherman@linux.com>
2025-03-10 17:38:58 +1000

    Adding some basic terraform code to test the CI/CD pipeline

---
+++ main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = "ap-southeast-2"  # Sydney region
+}
+
+# Get the latest Ubuntu 22.04 LTS AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get the default subnet in the first availability zone
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Create a security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+# Create EC2 instance
+resource "aws_instance" "ubuntu_free" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"  # Free tier eligible instance type
+  subnet_id     = tolist(data.aws_subnets.default.ids)[0]
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "Ubuntu-Free-Tier"
+  }
+
+  root_block_device {
+    volume_size = 8  # Free tier eligible storage
+    volume_type = "gp2"
+  }
+} 
