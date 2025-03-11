ab2b4f9fa1ae1cb794cf8c6077d87ab7aa065599
Roger Sherman <rsherman@linux.com>
2025-03-11 22:00:33 +1000

    FORMATTING!!! FMT!!!

--- backend/main.tf
+++ backend/main.tf
@@ -40,9 +40,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
